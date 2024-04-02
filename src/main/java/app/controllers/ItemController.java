00ff6a07bf5bbfd0883464fc54644722117b67d3
zPaddyz <jul2009@live.dk>
2024-04-02 16:20:15 +0200

    Basic version af ItemController færdiggjort

---
+++ src/main/java/app/controllers/ItemController.java
@@ -0,0 +1,28 @@
+package app.controllers;
+
+import app.exceptions.DatabaseException;
+import app.persistence.ConnectionPool;
+import app.persistence.ItemMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+public class ItemController {
+        public static void addRoutes(Javalin app, ConnectionPool connectionPool)
+        {
+            app.post("showBottom", ctx -> showBottom(ctx, connectionPool));
+            app.post("showTopping", ctx -> showTopping(ctx, connectionPool));
+        }
+
+    private static void showBottom(Context ctx, ConnectionPool connectionPool)
+    {
+        ItemMapper.showBottoms(connectionPool);
+        ctx.render("???.html");
+    }
+
+    private static void showTopping(Context ctx, ConnectionPool connectionPool)
+    {
+        ItemMapper.showToppings(connectionPool);
+        ctx.render("???.html");
+    }
+
+}
