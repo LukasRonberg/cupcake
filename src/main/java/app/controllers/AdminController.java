9d281728cf4d1f6025fae0e7ec756b0f4c3e3cdd
zPaddyz <jul2009@live.dk>
2024-04-03 14:10:47 +0200

    Færdiggjort AdminController, AdminMapper, adminSite.html. stylet og tilføjet funktionalitet til adminSite.html oprettet Order.java

--- src/main/java/app/controllers/AdminController.java
+++ src/main/java/app/controllers/AdminController.java
@@ -1,33 +1,66 @@
 package app.controllers;
 
+import app.entities.Order;
+import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.AdminMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.util.ArrayList;
+
 public class AdminController
 {
+
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
     {
-        app.post("getAllOrders", ctx -> getAllOrders(ctx, connectionPool));
-        app.post("showCustomer", ctx -> showCustomer(ctx, connectionPool));
+        app.get("/adminSite", ctx -> index(ctx));
+
+        app.post("/getAllOrders", ctx -> getAllOrders(ctx, connectionPool));
+        app.post("/showCustomer", ctx -> showCustomer(ctx, connectionPool));
+        app.post("/showCustomerOrders", ctx -> showCustomerOrders(ctx, connectionPool));
+
+        app.post("/addToBalance", ctx -> addMoneyToCustomerBalance(ctx, connectionPool));
+    }
+
+    private static void index(Context ctx)
+    {
+        ctx.render("adminSite.html");
     }
 
     private static void getAllOrders(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         AdminMapper.showAllOrders(connectionPool);
-        ctx.render("???.html");
+        ArrayList<Order> customerOrders = AdminMapper.showAllOrders(connectionPool);
+        ctx.attribute("customerOrders", customerOrders);
+
+        ctx.render("adminSite.html");
     }
 
     private static void showCustomer(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         String username = ctx.formParam("username_input");
-        AdminMapper.showCustomer(username, connectionPool);
-        ctx.render("???.html");
+        ArrayList<User> customer = AdminMapper.showCustomer(username, connectionPool);
+        ctx.attribute("customer", customer);
+
+        ctx.render("adminSite.html");
     }
 
     private static void showCustomerOrders(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         String username = ctx.formParam("username_input");
-        AdminMapper.showCustomerOrders(username, connectionPool);
-        ctx.render("???.html");
+
+        ArrayList<Order> customerOrders = AdminMapper.showCustomerOrders(username, connectionPool);
+        ctx.attribute("customerOrders", customerOrders);
+
+        ctx.render("adminSite.html");
+    }
+
+    private static void addMoneyToCustomerBalance(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
+        String customerEmail = ctx.formParam("email");
+        String amountToAdd = ctx.formParam("balance_input");
+
+        AdminMapper.addMoneyToCustomerBalance(customerEmail,amountToAdd, connectionPool);
+
+
+        ctx.render("adminSite.html");
     }
 }
