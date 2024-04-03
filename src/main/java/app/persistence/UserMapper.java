a668ee986d4969f109edfbee8e6920c436887cc7
LaCabraGrande <144101912+LaCabraGrande@users.noreply.github.com>
2024-04-03 14:28:56 +0200

    har lavet ændringer i UserController, UserMapper. Har oprettet User-klassen da jeg ikke havde den og ændret i createuser.html, index.html, Main.java og login.css

---
+++ src/main/java/app/persistence/UserMapper.java
@@ -0,0 +1,78 @@
+package app.persistence;
+
+import app.entities.User;
+import app.exceptions.DatabaseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper
+{
+
+    public static User login(String email, String password, ConnectionPool connectionPool) throws DatabaseException
+    {
+        String sql = "select * from public.\"users\" where email=? and password=?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        )
+        {
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next())
+            {
+                int id = rs.getInt("user_id");
+                boolean isAdmin = rs.getBoolean("is_admin");
+                String name = rs.getString("name");
+                String mobile = rs.getString("mobile");
+                int balance = rs.getInt("balance");
+                return new User(id, email, password, isAdmin, name, mobile, balance);
+            } else
+            {
+                throw new DatabaseException("Fejl i login. Prøv igen");
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("DB fejl", e.getMessage());
+        }
+    }
+
+    public static void createuser(String email, String password, String name, String mobile, ConnectionPool connectionPool) throws DatabaseException
+    {
+        String sql = "insert into users (email, password, is_admin, name, mobile, balance) values (?,?,?,?,?,?)";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        )
+        {
+            ps.setString(1, email);
+            ps.setString(2, password);
+            ps.setBoolean(3, false);
+            ps.setString(4, name);
+            ps.setString(5, mobile);
+            ps.setInt(6, '0');
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1)
+            {
+                throw new DatabaseException("Fejl ved oprettelse af ny bruger");
+            }
+        }
+        catch (SQLException e)
+        {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value "))
+            {
+                msg = "Brugernavnet findes allerede. Vælg et andet";
+            }
+            throw new DatabaseException(msg, e.getMessage());
+        }
+    }
+}
