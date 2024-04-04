7132d6d2b5465c61f25b8a2eb22cab52db7034d7
LukasRonberg <143714608+LukasRonberg@users.noreply.github.com>
2024-04-04 13:24:18 +0200

    Har cleanet main lidt op

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,22 +2,14 @@ package app;
 
 
 import app.config.ThymeleafConfig;
+import app.controllers.ItemController;
 import app.controllers.UserController;
-import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
 
 
 public class Main
 {
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
-    private static  final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "cupcake";
-
-
-    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
-
     public static void main(String[] args) {
 
         // Initializing Javalin and Jetty webserver
@@ -30,9 +22,9 @@ public class Main
 
         //Routing
 
+        ItemController.addRoutes(app);
 
-        app.get("/", ctx -> ctx.render("index.html"));
-        UserController.addRoutes(app, connectionPool);
+        UserController.addRoutes(app);
 
 
     }
